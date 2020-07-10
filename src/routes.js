75a5381d5a5e7b89ae748a67504b335e5ad1fa8e
Whalyson Ramos <warius18@gmail.com>
2020-07-10 01:40:48 -0300

    Finalizando Mini Curso RocketSeat - NODE API

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const routes = express.Router();
+const ProductsController = require('./controllers/ProductController')
+
+routes.get('/products', ProductsController.index);
+routes.post('/products', ProductsController.store);
+routes.get('/products/:id', ProductsController.show);
+routes.put('/products/:id', ProductsController.update);
+routes.delete('/products/:id', ProductsController.remove);
+module.exports = routes;
